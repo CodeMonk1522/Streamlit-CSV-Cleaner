2e1153a2fd72aaa85bdeca4f564e45e3b2e52cb4
Shubham <shubham@Shubhams-MBP.cable.rcn.com>
2024-03-05 13:27:34 -0600

    added button

--- fbc_csvCleaner.py
+++ fbc_csvCleaner.py
@@ -1,4 +1,5 @@
 from base64 import b64encode
+import base64
 import streamlit as st
 import pandas as pd
 
@@ -33,11 +34,12 @@ def main():
         
         # Download link
         # st.markdown(get_download_link(filtered_df), unsafe_allow_html=True)
-        st.button("Download Filtered Sheet", get_download_link(filtered_df), unsafe_allow_html=True)
+        st.download_button(label="Download Filtered Sheet", data=get_download_link(filtered_df), file_name='data.csv', mime='text/csv')
 
 def get_download_link(df):
     csv = df.to_csv(index=False)
-    href = f'<a href="data:file/csv;base64,{b64encode(csv.encode()).decode()}" download="filtered_data.csv">Download Filtered CSV File</a>'
+    b64 = base64.b64encode(csv.encode()).decode()  # Bypass pandas to write directly to string
+    href = f'<a href="data:file/csv;base64,{b64}" download="filtered_data.csv">Download CSV File</a>'
     return href
 
 if __name__ == "__main__":
